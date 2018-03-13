f5ea589c0bfc9c7d638ab77ee254228b75a24b00
tehilbi <tehilabitarov@gmail.com>
2018-03-13 19:24:17 +0200

    color red-test

--- App.js
+++ App.js
@@ -45,7 +45,7 @@ export default class App extends Component {
         </Text>
         <View style={styles.inputContainer}>  
            <TouchableOpacity
-              style={{height:220,backgroundColor:'black',justifyContent:'center',width:'100%'}}>
+              style={{height:220,backgroundColor:'red',justifyContent:'center',width:'100%'}}>
               <Text style={styles.Findpark}>
               {this.state.Findpark}
               </Text>
